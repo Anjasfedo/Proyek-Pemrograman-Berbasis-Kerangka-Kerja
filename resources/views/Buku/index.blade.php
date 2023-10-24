0ca4ab6fdce6be11193ab1629ec0bcf1f72ff203
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-10-24 13:10:54 +0700

    PPBKK Minggu 3

--- resources/views/Buku/index.blade.php
+++ resources/views/Buku/index.blade.php
@@ -7,30 +7,48 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <div class="card mx-auto">
-            <div class="card-body text-center">
-                <h1>Daftar Buku</h1>
-                <table class="table table-bordered">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Penulis</th>
-                        <th scope="col">Judul</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      @foreach ($buku as $key => $item)
-                        <tr>
-                          <th scope="row">{{ $item['id'] }}</th>
-                          <td>{{ $item['penulis'] }}</td>
-                          <td>{{ $item['judul'] }}</td>
-                        </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
-            </div>
+    <div class="container">
+        <h1>Daftar Buku</h1>
+
+        @if (session('pesan'))
+        <div class="alert alert-success">
+            {{ session('pesan') }}
         </div>
+        @endif
+
+        <a href="{{ route('buku.create') }}" class="btn btn-primary">Tambah</a>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Judul</th>
+                    <th>Penulis</th>
+                    <th>Deskripsi</th>
+                    <th>Detail</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($buku as $item)
+                <tr>
+                    <td>{{ $item->title }}</td>
+                    <td>{{ $item->author }}</td>
+                    <td>{{ $item->description }}</td>
+                    <td>
+                      <a href="{{ route('buku.show', [$item->id]) }}" class="btn btn-success">Detail</a>
+                    </td>
+                    <td>
+                      <a href="{{ route('buku.edit', [$item->id]) }}" class="btn btn-primary">Edit</a>
+                      <form action="{{ route('buku.destroy', [$item->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger" onclick="return confirm('Yakin ingin menghapus buku ini?')">Hapus</button>
+                      </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
