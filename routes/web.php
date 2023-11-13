3b644eeb51326e4136cbaaf9a1456de47b2944bc
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-11-13 22:12:18 +0700

    Initialize CoreUi

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,15 @@ Route::get('/mahasiswa/{npm}', [MahasiswaController::class, 'index']);
 Route::resource('/book', BookController::class);
 
 Route::resource('/author', AuthorController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::view('about', 'about')->name('about');
+
+    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+
+    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+});
